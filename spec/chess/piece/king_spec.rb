c5c621c060d873d42ee02828bba4b2b3241a67f6
fengb <benjamin.feng@gmail.com>
2013-02-27 11:14:27 -0600

    Added real up 1 test

--- spec/chess/piece/king_spec.rb
+++ spec/chess/piece/king_spec.rb
@@ -5,7 +5,7 @@ describe Chess::Piece::King do
   describe '.available_moves' do
     it 'can move up 1' do
       p = Chess::Piece::King.new
-      p.available_moves
+      p.available_moves.include?(1).should be_true
     end
   end
 end
