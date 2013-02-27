e76a5a500e4b452c921239c38e6aaed108e661be
Elom Gomez <egomez@enova.com>
2013-02-27 14:04:52 -0600

    Funtionality check for origin movement

--- lib/chess/piece/queen.rb
+++ lib/chess/piece/queen.rb
@@ -3,6 +3,7 @@ module Chess
 		class Queen
 
 			def can_move?(x,y)
+				return false if x.zero? and y.zero?
 				return true if x == 0 
 				return true if y == 0
 				return [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]].include?([x,y])
