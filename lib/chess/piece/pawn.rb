9db36ae44c48bbe6050558800416ac70866c58aa
Elom Gomez <egomez@enova.com>
2013-02-27 15:33:58 -0600

    pawns can move forwards twice on first move

--- lib/chess/piece/pawn.rb
+++ lib/chess/piece/pawn.rb
@@ -7,8 +7,11 @@ module Chess
 			end
 
 			def can_move?(x,y)
-				return true if (x == 0  and y == 1) and @up_or_down == :up
+				return true if (x == 0  and y == 1) and @up_or_down == :up 
 				return true if (x == 0  and y == -1) and @up_or_down == :down
+				return true if (x == 0  and y == 2) and @up_or_down == :up and first_move?
+				return true if (x == 0  and y == -2) and @up_or_down == :down and first_move?
+				
 			end
 
 			def first_move?
