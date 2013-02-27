08b027e0c25e7257596f8b697e27c3c035372100
Elom Gomez <egomez@enova.com>
2013-02-27 13:59:49 -0600

    Queen's method can_move now accepts two variables

--- lib/chess/piece/queen.rb
+++ lib/chess/piece/queen.rb
@@ -2,9 +2,9 @@ module Chess
 	class Piece
 		class Queen
 
-			def can_move?(move)
-				return true if move[0] == 0 
-				return [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]].include?(move)
+			def can_move?(x,y)
+				return true if x == 0 
+				return [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]].include?([x,y])
 			end
 
 		end
