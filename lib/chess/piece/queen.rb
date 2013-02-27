e4a150e5ade0d50eb412d3e601ed6cd5b59b6734
Elom Gomez <egomez@enova.com>
2013-02-27 13:47:47 -0600

    Changed available_move method to can_move? for Queen

--- lib/chess/piece/queen.rb
+++ lib/chess/piece/queen.rb
@@ -2,8 +2,8 @@ module Chess
 	class Piece
 		class Queen
 
-			def available_moves
-				return [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]]
+			def can_move?(move)
+				return [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]].include?(move)
 			end
 
 		end
