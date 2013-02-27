20f36ad3a34fd75bf160fc76b60852c6a581a3c2
fengb <benjamin.feng@gmail.com>
2013-02-27 13:44:19 -0600

    Updated queen to use can_move? instead of available moves

--- spec/chess/piece/queen_spec.rb
+++ spec/chess/piece/queen_spec.rb
@@ -2,30 +2,30 @@ require 'chess/piece/queen'
 
 
 describe Chess::Piece::Queen do
-  describe '.available_moves' do
+  describe '.can_move?' do
     let(:queen) { Chess::Piece::Queen.new }
 
     it 'can move up 1' do
-      queen.available_moves.include?([0, 1]).should be_true
+      queen.can_move?([0, 1]).should be_true
     end
 
     it 'can move down 1' do
-      queen.available_moves.include?([0, -1]).should be_true
+      queen.can_move?([0, -1]).should be_true
     end
 
     it 'can move right 1' do
-      queen.available_moves.include?([1, 0]).should be_true
+      queen.can_move?([1, 0]).should be_true
     end
 
     it 'can move left 1' do
-      queen.available_moves.include?([-1, 0]).should be_true
+      queen.can_move?([-1, 0]).should be_true
     end
 
     it 'can move diagonals' do
-      queen.available_moves.include?([ 1,  1]).should be_true
-      queen.available_moves.include?([-1, -1]).should be_true
-      queen.available_moves.include?([ 1, -1]).should be_true
-      queen.available_moves.include?([-1,  1]).should be_true
+      queen.can_move?([ 1,  1]).should be_true
+      queen.can_move?([-1, -1]).should be_true
+      queen.can_move?([ 1, -1]).should be_true
+      queen.can_move?([-1,  1]).should be_true
     end
   end
 end
