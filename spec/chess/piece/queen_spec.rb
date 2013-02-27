079b08b56cbd3bdd7f38769aa7c34b3a7af086c0
fengb <benjamin.feng@gmail.com>
2013-02-27 13:58:16 -0600

    Simplified method call

--- spec/chess/piece/queen_spec.rb
+++ spec/chess/piece/queen_spec.rb
@@ -6,31 +6,31 @@ describe Chess::Piece::Queen do
     let(:queen) { Chess::Piece::Queen.new }
 
     it 'can move up 1' do
-      queen.can_move?([0, 1]).should be_true
+      queen.can_move?(0, 1).should be_true
     end
 
     it 'can move down 1' do
-      queen.can_move?([0, -1]).should be_true
+      queen.can_move?(0, -1).should be_true
     end
 
     it 'can move right 1' do
-      queen.can_move?([1, 0]).should be_true
+      queen.can_move?(1, 0).should be_true
     end
 
     it 'can move left 1' do
-      queen.can_move?([-1, 0]).should be_true
+      queen.can_move?(-1, 0).should be_true
     end
 
     it 'can move diagonals' do
-      queen.can_move?([ 1,  1]).should be_true
-      queen.can_move?([-1, -1]).should be_true
-      queen.can_move?([ 1, -1]).should be_true
-      queen.can_move?([-1,  1]).should be_true
+      queen.can_move?( 1,  1).should be_true
+      queen.can_move?(-1, -1).should be_true
+      queen.can_move?( 1, -1).should be_true
+      queen.can_move?(-1,  1).should be_true
     end
 
     it 'can move up as much as she wants' do
       (1..10000).each do |num|
-        queen.can_move?([0, num]).should be_true
+        queen.can_move?(0, num).should be_true
       end
     end
   end
