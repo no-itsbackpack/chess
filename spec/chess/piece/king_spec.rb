61586ae002329e4efe5721f8135fa3648b23d8e0
fengb <benjamin.feng@gmail.com>
2013-02-27 11:21:05 -0600

    Moved :king as a let block

--- spec/chess/piece/king_spec.rb
+++ spec/chess/piece/king_spec.rb
@@ -3,21 +3,20 @@ require 'chess/piece/king'
 
 describe Chess::Piece::King do
   describe '.available_moves' do
+    let(:king) { Chess::Piece::King.new }
+
     it 'can move up 1' do
-      p = Chess::Piece::King.new
-      p.available_moves.include?(1).should be_true
+      king.available_moves.include?(1).should be_true
     end
 
     it 'cannot move up more than 1' do
-      p = Chess::Piece::King.new
       [2, 10, 100].each do |num|
-        p.available_moves.include?(num).should be_false
+        king.available_moves.include?(num).should be_false
       end
     end
 
     it 'can move down 1' do
-      p = Chess::Piece::King.new
-      p.available_moves.include?(-1).should be_true
+      king.available_moves.include?(-1).should be_true
     end
   end
 end
