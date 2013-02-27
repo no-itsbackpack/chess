79b4126cfe8c102a6ea81da55c7462e9e028223c
fengb <benjamin.feng@gmail.com>
2013-02-27 11:25:37 -0600

    Updated available_moves signature to use [x, y]

--- spec/chess/piece/king_spec.rb
+++ spec/chess/piece/king_spec.rb
@@ -6,22 +6,22 @@ describe Chess::Piece::King do
     let(:king) { Chess::Piece::King.new }
 
     it 'can move up 1' do
-      king.available_moves.include?(1).should be_true
+      king.available_moves.include?([0, 1]).should be_true
     end
 
     it 'cannot move up more than 1' do
       [2, 10, 100].each do |num|
-        king.available_moves.include?(num).should be_false
+        king.available_moves.include?([0, num]).should be_false
       end
     end
 
     it 'can move down 1' do
-      king.available_moves.include?(-1).should be_true
+      king.available_moves.include?([0, -1]).should be_true
     end
 
     it 'cannot move down more than 1' do
       [-2, -10, -100].each do |num|
-        king.available_moves.include?(num).should be_false
+        king.available_moves.include?([0, num]).should be_false
       end
     end
   end
