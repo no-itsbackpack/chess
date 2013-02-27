6634fee439207a70572ccefd8cf883b69cc17b18
fengb <benjamin.feng@gmail.com>
2013-02-27 14:23:52 -0600

    Added rook test

---
+++ spec/chess/piece/rook_spec.rb
@@ -0,0 +1,54 @@
+require 'chess/piece/rook'
+
+
+describe Chess::Piece::Rook do
+  describe '.can_move?' do
+    let(:rook) { Chess::Piece::Rook.new }
+
+    it 'cannot move to origin' do
+      rook.can_move?(0, 0).should be_false
+    end
+
+    it 'can move up as much as he wants' do
+      (1..10000).each do |num|
+        rook.can_move?(0, num).should be_true
+      end
+    end
+
+    it 'can move down as much as he wants' do
+      (1..10000).each do |num|
+        rook.can_move?(0, -num).should be_true
+      end
+    end
+
+    it 'can move right as much as he wants' do
+      (1..10000).each do |num|
+        rook.can_move?(num, 0).should be_true
+      end
+    end
+
+    it 'can move left as much as he wants' do
+      (1..10000).each do |num|
+        rook.can_move?(-num, 0).should be_true
+      end
+    end
+
+    it 'cannot move diagonals' do
+      (1..10000).each do |num|
+        rook.can_move?( num,  num).should be_false
+        rook.can_move?(-num,  num).should be_false
+        rook.can_move?( num, -num).should be_false
+        rook.can_move?(-num, -num).should be_false
+      end
+    end
+
+    it 'cannot move anywhere else' do
+      [2, 3, 5, 7, 9].each do |i|
+        [4, 6, 8, 10, 12].each do |j|
+          rook.can_move?(i, j).should be_false
+          rook.can_move?(j, i).should be_false
+        end
+      end
+    end
+  end
+end
