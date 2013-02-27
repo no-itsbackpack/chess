41a64f2885c82b559233e864f930748644416ef2
fengb <benjamin.feng@gmail.com>
2013-02-27 14:03:26 -0600

    Added falsy origin movement

--- spec/chess/piece/queen_spec.rb
+++ spec/chess/piece/queen_spec.rb
@@ -5,6 +5,10 @@ describe Chess::Piece::Queen do
   describe '.can_move?' do
     let(:queen) { Chess::Piece::Queen.new }
 
+    it 'cannot move to origin' do
+      queen.can_move?(0, 0).should be_false
+    end
+
     it 'can move up as much as she wants' do
       (1..10000).each do |num|
         queen.can_move?(0, num).should be_true
