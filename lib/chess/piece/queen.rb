bf30099cc649db3be62abaee72504e33f21f4e2f
Elom Gomez <egomez@enova.com>
2013-02-27 14:15:46 -0600

    Queen cannot make incorrect moves

--- lib/chess/piece/queen.rb
+++ lib/chess/piece/queen.rb
@@ -3,10 +3,10 @@ module Chess
 		class Queen
 
 			def can_move?(x,y)
-				return false if x.zero? and y.zero?
+				return false if (x.zero? and y.zero?)
 				return true if x == 0 
 				return true if y == 0
-				return true if !x.zero? and !y.zero?
+				return true if (x.abs == y.abs)
 			end
 
 		end
