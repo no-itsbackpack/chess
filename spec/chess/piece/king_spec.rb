54a5d44a7c0c20c491b0a10738968add2550d2fc
fengb <benjamin.feng@gmail.com>
2013-02-27 14:20:07 -0600

    Updated king method signature on test

--- spec/chess/piece/king_spec.rb
+++ spec/chess/piece/king_spec.rb
@@ -6,35 +6,35 @@ describe Chess::Piece::King do
     let(:king) { Chess::Piece::King.new }
 
     it 'can move up 1' do
-      king.available_moves.include?([0, 1]).should be_true
+      king.can_move?(0, 1).should be_true
     end
 
     it 'can move down 1' do
-      king.available_moves.include?([0, -1]).should be_true
+      king.can_move?(0, -1).should be_true
     end
 
     it 'can move right 1' do
-      king.available_moves.include?([1, 0]).should be_true
+      king.can_move?(1, 0).should be_true
     end
 
     it 'can move left 1' do
-      king.available_moves.include?([-1, 0]).should be_true
+      king.can_move?(-1, 0).should be_true
     end
 
     it 'can move diagonals' do
-      king.available_moves.include?([ 1,  1]).should be_true
-      king.available_moves.include?([-1, -1]).should be_true
-      king.available_moves.include?([ 1, -1]).should be_true
-      king.available_moves.include?([-1,  1]).should be_true
+      king.can_move?( 1,  1).should be_true
+      king.can_move?(-1, -1).should be_true
+      king.can_move?( 1, -1).should be_true
+      king.can_move?(-1,  1).should be_true
     end
 
     it 'cannot move anywhere else' do
       [0, 2, 10, 100].each do |x|
         [0, 2, 10, 100].each do |y|
-          king.available_moves.include?([ x,  y]).should be_false
-          king.available_moves.include?([-x, -y]).should be_false
-          king.available_moves.include?([ x, -y]).should be_false
-          king.available_moves.include?([-x,  y]).should be_false
+          king.can_move?( x,  y).should be_false
+          king.can_move?(-x, -y).should be_false
+          king.can_move?( x, -y).should be_false
+          king.can_move?(-x,  y).should be_false
         end
       end
     end
